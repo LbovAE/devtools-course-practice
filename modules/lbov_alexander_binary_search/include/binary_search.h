5f4d54bc52ee94d4130bac75efadbfc9d0672301
unknown <aleksandr2001god@gmail.com>
2022-10-27 13:09:05 +0300

    Version2.4

---
+++ modules/lbov_alexander_binary_search/include/binary_search.h
@@ -0,0 +1,37 @@
+// Copyright 2022 Lbov Alexander
+
+#ifndef MODULES_LBOV_ALEXANDER_BINARY_SEARCH_INCLUDE_BINARY_SEARCH_H_
+#define MODULES_LBOV_ALEXANDER_BINARY_SEARCH_INCLUDE_BINARY_SEARCH_H_
+
+#include <vector>
+#include <string>
+
+template <class T>
+class BinarySearch {
+ public:
+    BinarySearch() {}
+    ~BinarySearch() {}
+    int BinSearch(T key, const std::vector<T>& array) {
+        int left = 0;
+        int rigtn = static_cast<int>(array.size()) - 1;
+        int middle = (left + right) / 2;
+        bool flag = false;
+        while (left <= right && !flag) {
+            if (array[middle] == key) {
+                flag = true;
+            } else if (array[middle] > key) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+            middle = (left + right) / 2;
+        }
+        if (flag) {
+            return middle;
+        } else {
+            return -1;
+        }
+    }
+};
+
+#endif  // MODULES_LBOV_ALEXANDER_BINARY_SEARCH_INCLUDE_BINARY_SEARCH_H_
---
+++ modules/lbov_alexander_binary_search/test/test_binary_search.cpp
@@ -0,0 +1,53 @@
+// Copyright 2022 Lbov Alexander
+
+#include <gtest/gtest.h>
+
+#include <vector>
+#include <string>
+
+#include "include/binary_search.h"
+
+TEST(Lbov_Alexander_Binary_Search_Test, can_find_int) {
+    std::vector<int> array = {
+        -5, -3, -1, 0, 1, 3, 5
+    };
+    BinarySearch<int> b;
+    int result = b.BinSearch(0, array);
+    ASSERT_EQ(3, result);
+}
+
+TEST(Lbov_Alexander_Binary_Search_Test, can_find_double) {
+    std::vector<double> array = {
+        -5.0, -3.0, -1.0, 0.0, 1.0, 3.0, 5.0
+    };
+    BinarySearch<double> b;
+    int result = b.BinSearch(1.0, array);
+    ASSERT_EQ(4, result);
+}
+
+TEST(Lbov_Alexander_Binary_Search_Test, can_find_string) {
+    std::vector<std::string> array = {
+        "a", "b", "c", "d", "e", "f"
+    };
+    BinarySearch<std::string> b;
+    int result = b.BinSearch("b", array);
+    ASSERT_EQ(1, result);
+}
+
+TEST(Lbov_Alexander_Binary_Search_Test, correct_work_on_missing_value) {
+    std::vector<int> array = {
+        -5, -3, -1, 0, 1, 3, 5
+    };
+    BinarySearch<int> b;
+    int result = b.BinSearch(1337, array);
+    ASSERT_EQ(-1, result);
+}
+
+TEST(Lbov_Alexander_Binary_Search_Test, search_in_unsorted) {
+    std::vector<int> array = {
+        2, 1, 4, 3, 6, 5, 0
+    };
+    BinarySearch<int> b;
+    int result = b.BinSearch(4, array);
+    ASSERT_EQ(-1, result);
+}
